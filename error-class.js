c26bef964481609d7a4bc81a996c0330e60d19b9
Sanjeev Parashiva <Sanjeev.Parashiva@in.tesco.com>
2017-07-27 18:56:13 +0530

    add promise examples

--- error-class.js
+++
@@ -1,9 +0,0 @@
-let error = new Error({"err_message":"New Error"});
-console.log(error.message);
-console.log(Object.getOwnPropertyNames(error));
-console.log(error.stack);
-console.log(error.code);
-
-
-let refError = new ReferenceError()
-console.log(refError);
---
+++ test-promise.js
@@ -0,0 +1,11 @@
+function add(num1,num2){
+  return new Promise((resolve,reject)=>{
+
+  })
+}
+
+add(1,2).then(result =>{
+  console.log(result);
+}).catch(reason=>{
+  console.log(reason);
+})
