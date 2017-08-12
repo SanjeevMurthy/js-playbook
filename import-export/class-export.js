05c9fffd8f1fa6f9a9c7884664f984cf4e7b10f3
Sanjeev Parashiva <Sanjeev.Parashiva@in.tesco.com>
2017-08-12 22:17:59 +0530

    update import/export

---
+++ import-export/class-export.js
@@ -0,0 +1,23 @@
+class Class1{
+  constructor(message){
+    this.message = message;
+  }
+
+  get(){
+    return this.message;
+  }
+}
+
+
+class Class2{
+  constructor(message){
+    this.message = message;
+  }
+
+  get(){
+    return this.message;
+  }
+}
+
+
+module.exports =  new Class1('CLASS1');
--- import-export/index.js
+++ import-export/index.js
@@ -1,14 +1,30 @@
-import anything from './default';
-import {funcA} from './module-exports';
-//import whatthing from './hello.js';
-//import {funcA} from './hello'
+// import anything from './default';
+// import {funcA} from './module-exports';
+// //import whatthing from './hello.js';
+// //import {funcA} from './hello'
+//
+// anything();
+//
+// //console.log(something);
+//
+// //console.log(whatthing);
+// console.log(funcA);
+//
+// //whatthing();
+// funcA
 
-anything();
+// import { Class1, Class2} from './class-export';
+// // import Class1 from './class-export';
+// // import Class2 from './class-export';
+//
+// console.log(Class2);
+//
+// let cls2 = new Class2('CLASS-2');
+// let cls1 = new Class1('CLASS-1');
+//
+// console.log(cls1.get());
+// console.log(cls2.get());
 
-//console.log(something);
+import cls from './class-export';
 
-//console.log(whatthing);
-console.log(funcA);
-
-//whatthing();
-funcA
+console.log(cls.get());
